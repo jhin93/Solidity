437eb63180cd4a1f38298c1490a7275d02cea2be
jhin93 <scene1993@gmail.com>
2021-04-08 22:24:06 +0900

    오류 해결 및 SPDX

--- first.sol
+++ first.sol
@@ -1,13 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+// 컨트랙트는 오픈소스를 기본으로 하기 때문에 라이센스를 명시해 주는 것이 좋습니다. 솔리디티는 주석으로 SPDX 방법에 따라 라이센스를 작성합니다. 
+// 오픈소스가 아니거나 라이센스를 명시하지 않고 싶을 때는 UNLICENSED로 작성합니다. 
+// http://www.umlcert.com/ethereum-dapps-11/
 
-// Solidity 1일차
+// SPDX란? 
+// https://en.wikipedia.org/wiki/Software_Package_Data_Exchange
+// https://www.olis.or.kr/license/licenseSPDX.do?mapcode=010107&page=1
+
 // 스마트 컨트랙트 소개.
 // 간단한 스마트 컨트랙트
 // https://solidity-kr.readthedocs.io/ko/latest/introduction-to-smart-contracts.html
 
-pragma solidity >=0.4.0 < 0.6.0;
+pragma solidity ^0.7.0;
 
 contract SimpleStorage {
-    uint storedDta;
+    uint storedData;
 
     function set(uint x) public {
         storedData = x;
