86566cbf451ca0b8ad641f4eae8daa28c63d3f88
jhin93 <scene1993@gmail.com>
2021-04-08 22:39:48 +0900

    spdx 설명 추가

--- first.sol
+++ first.sol
@@ -4,6 +4,8 @@
 // http://www.umlcert.com/ethereum-dapps-11/
 
 // SPDX란? 
+// 소프트웨어 패키지 데이터 교환(SPDX)은 주어진 컴퓨터 소프트웨어 가 배포 되는 소프트웨어 라이센스 에 대한 정보를 문서화하는 데 사용되는 파일 형식 입니다. 
+// SPDX는 Linux Foundation 의 후원하에 20 개 이상의 서로 다른 조직을 대표하는 SPDX 워킹 그룹에 의해 작성되었습니다 
 // https://en.wikipedia.org/wiki/Software_Package_Data_Exchange
 // https://www.olis.or.kr/license/licenseSPDX.do?mapcode=010107&page=1
 
