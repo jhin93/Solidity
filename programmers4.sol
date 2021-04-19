0e67c8f9aaf514dd8cb4924aba02ea6aef730f13
jhin93 <scene1993@gmail.com>
2021-04-19 20:09:39 +0900

    프로그래머스 파트 2. 이더리움 송금하기

---
+++ programmers4.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.24;
+
+contract Solution {
+    uint256 balance = 0;
+
+    // payable 키워드를 넣어 함수를 완성하세요
+    function sending() /* */ public {
+        balance = msg.value;
+        Sended(msg.value, balance);
+    }
+
+    //로깅을 위한 이벤트입니다. 삭제하지 마세요,
+    event Sended(
+        uint256 _value,
+        uint256 _balance
+    );
+}
