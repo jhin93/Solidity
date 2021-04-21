1b66871481d2aeb137d257d3d59a737dbd99621d
jhin93 <scene1993@gmail.com>
2021-04-21 22:02:01 +0900

    chapter7 풀것. chapter 6 getter, setter 정리

---
+++ cryptoZombie/chapter7.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.0;
+
+
+contract ZombieFactory {
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    // 여기서 시작
+
+}
