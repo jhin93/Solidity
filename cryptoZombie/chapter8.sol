13985975676a4feff0f5a7a496997fc8d55d9b97
jhin93 <scene1993@gmail.com>
2021-04-25 14:40:39 +0900

    struct의 배열에 인자추가

---
+++ cryptoZombie/chapter8.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.0;
+
+contract ZombieFactory {
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    function createZombie(string _name, uint _dna) {
+        // 여기서 시작
+        zombies.push(_name, _dna);
+    }
+
+}
