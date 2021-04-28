2f393d02e38a432a5f8acee00e761cb1f86b0e18
jhin93 <scene1993@gmail.com>
2021-04-28 21:11:23 +0900

    private 함수는 인자처럼 _를 앞에 붙여서 이름짓는게 관례.

--- cryptoZombie/chapter9.sol
+++ cryptoZombie/chapter9.sol
@@ -12,12 +12,15 @@ contract ZombieFactory {
     }
 
     Zombie[] public zombies;
-    
+
     function _createZombie(string _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
     }
 
 }
 
+// private는 컨트랙트 내의 다른 함수들만이 이 함수를 호출하여 numbers 배열로 무언가를 추가할 수 있다는 것을 의미하지.
+
 // 위의 예시에서 볼 수 있듯이 private 키워드는 함수명 다음에 적네. 
+
 // 함수 인자명과 마찬가지로 private한 함수명도 언더바(_)로 시작하는 것이 관례라네.
