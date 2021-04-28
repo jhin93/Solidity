13647466d14392e74870d6a65a20a097cc3ae97b
jhin93 <scene1993@gmail.com>
2021-04-28 21:47:53 +0900

    구조체, 배열 또는 매핑 등의 모든 변수를 위해 데이터 위치를 명시하는 것이 필수.

--- cryptoZombie/chapter9.sol
+++ cryptoZombie/chapter9.sol
@@ -21,7 +21,7 @@ contract ZombieFactory {
 
 // 의문점 해결.
 // https://ethereum.stackexchange.com/questions/63294/typeerror-data-location-must-be-storage-or-memory-for-parameter-in-function
-// solidity 0.5.0 버전부터는 구조체, 배열 또는 매핑 유형의 모든 변수에 대한 명시적 데이터 위치가 필수적입니다.
+// solidity 0.5.0 버전부터는 구조체, 배열 또는 매핑 등의 모든 변수를 위해 데이터 위치를 명시하는 것이 필수.
 
 // private는 컨트랙트 내의 다른 함수들만이 이 함수를 호출하여 numbers 배열로 무언가를 추가할 수 있다는 것을 의미하지.
 // 위의 예시에서 볼 수 있듯이 private 키워드는 함수명 다음에 적네. 
