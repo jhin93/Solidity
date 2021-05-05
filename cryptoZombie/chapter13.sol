2746442c88a3231308b02aca598414170449e075
jhin93 <scene1993@gmail.com>
2021-05-05 21:48:18 +0900

    오류해결

--- cryptoZombie/chapter13.sol
+++ cryptoZombie/chapter13.sol
@@ -16,14 +16,19 @@ contract ZombieFactory {
 
     function _createZombie(string memory _name, uint _dna) private {
         // 새롭게 Zombie 구조체에 추가를 한 좀비의 인덱스를 id로 사용한다.
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombies.push(Zombie(_name, _dna));
+        uint id = zombies.length - 1;
         // uint id = zombies.push(Zombie(_name, _dna)) - 1;
         // 위처럼 사용할 시 다음과 같은 오류 발생. Different number of components on the left hand side (1) than on the right hand side
         // 크립토좀비에선 문제가 없었던 걸 보니 버전차이로 추정. 
-        
-        // 여기서 이벤트 실행
-        NewZombie(id, _name, _dna);
+        // 해결. https://ethereum.stackexchange.com/questions/89792/typeerror-different-number-of-components-either-side-of-equation
+        // 버전 0.6부터 push가 length를 반환하지 않고 더하기 기능만 수행함. 
 
+        // 여기서 이벤트 실행
+        emit NewZombie(id, _name, _dna);
+        // 오류발생. Event invocations have to be prefixed by "emit".
+        // 해결. https://ethereum.stackexchange.com/questions/45482/invoking-events-without-emit-prefix-is-deprecated-in-transfermsg-sender-to/45485
+        // 트랜잭션 로그에 이벤트 데이터를 집어넣기 위해선 emit키워드를 사용한다. https://has3ong.tistory.com/393
     }
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
