a95925d1a4bce94bfe973c2380c3a401521776a5
jhin93 <scene1993@gmail.com>
2021-05-08 18:06:45 +0900

    복습리딩

--- cryptoZombie/course2/chapter3.sol
+++ cryptoZombie/course2/chapter3.sol
@@ -57,19 +57,19 @@ contract ZombieFactory {
 
 // ※ msg.sender를 이용하고 mapping을 업데이트하는 예시.
 
-mapping (address => uint) favoriteNumber;
+// mapping (address => uint) favoriteNumber;
 
-function setMyNumber(uint _myNumber) public {
+// function setMyNumber(uint _myNumber) public {
   // `msg.sender`에 대해 `_myNumber`가 저장되도록 `favoriteNumber` 매핑을 업데이트한다 `
-  favoriteNumber[msg.sender] = _myNumber;
+  // favoriteNumber[msg.sender] = _myNumber;
   // ^ 데이터를 저장하는 구문은 배열로 데이터를 저장할 떄와 동일하다 
-}
+// }
 
-function whatIsMyNumber() public view returns (uint) {
+// function whatIsMyNumber() public view returns (uint) {
   // sender의 주소에 저장된 값을 불러온다 
   // sender가 `setMyNumber`을 아직 호출하지 않았다면 반환값은 `0`이 될 것이다
-  return favoriteNumber[msg.sender];
-}
+  // return favoriteNumber[msg.sender];
+// }
 // 이 간단한 예시에서 누구나 setMyNumber을 호출하여 본인의 주소와 연결된 우리 컨트랙트 내에 uint를 저장할 수 있지.
 
 
