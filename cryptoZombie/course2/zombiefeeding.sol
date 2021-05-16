c04bc653302156a695a16a1992ed4873175f8529
jhin93 <scene1993@gmail.com>
2021-05-16 21:58:08 +0900

    lesson2 chapter 7: storage vs memory 1

--- cryptoZombie/course2/zombiefeeding.sol
+++ cryptoZombie/course2/zombiefeeding.sol
@@ -1,18 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.7.0;
 
-// 챕터 6: Import
+// 챕터 7: Storage vs Memory
+// 솔리디티에는 변수를 저장할 수 있는 공간으로 storage와 memory 두 가지가 있지.
 
-// 우리 코드가 꽤 길어지고 있으니, 여러 파일로 나누어 정리하면 관리하기 더 편하겠지. 
-// 보통 이런 방식으로 솔리디티 프로젝트의 긴 코드를 처리할 것이네.
-// 다수의 파일이 있고 어떤 파일을 다른 파일로 불러오고 싶을 때, 솔리디티는 import라는 키워드를 이용하지.
+// Storage는 블록체인 상에 영구적으로 저장되는 변수를 의미하지. 
+// Memory는 임시적으로 저장되는 변수로, 컨트랙트 함수에 대한 외부 호출들이 일어나는 사이에 지워지지. 
+// 두 변수는 각각 컴퓨터 하드 디스크와 RAM과 같지.
 
-// 예시
-// import "./someothercontract.sol";
-// contract newContract is SomeOtherContract {
-// }
-
-// 이 컨트랙트와 동일한 폴더에 (이게 ./가 의미하는 바임) someothercontract.sol이라는 파일이 있을 때, 이 파일을 컴파일러가 불러오게 되지.
+// 대부분의 경우에 자네는 이런 키워드들을 이용할 필요가 없네. 
+// 왜냐면 솔리디티가 알아서 처리해 주기 때문이지. 
+// 상태 변수(함수 외부에 선언된 변수)는 초기 설정상 storage로 선언되어 블록체인에 영구적으로 저장되는 반면, 
+// 함수 내에 선언된 변수는 memory로 자동 선언되어서 함수 호출이 종료되면 사라지지.
 
 // 여기에 import 구문을 넣기
 import "./zombiefactory.sol";
