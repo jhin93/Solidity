608e662c5b2bbe914b18e362b6bb933685f80f72
jhin93 <scene1993@gmail.com>
2021-05-28 16:59:39 +0900

    lesson 3 챕터 1: 컨트랙트의 불변성 2

--- cryptoZombie/course2/zombiefeeding.sol
+++ cryptoZombie/course2/zombiefeeding.sol
@@ -6,6 +6,32 @@ pragma solidity ^0.7.0;
 
 // 챕터 1: 컨트랙트의 불변성
 
+// 지금까지 본 것만으로는, 솔리디티는 자바스크립트 같은 다른 언어와 꽤 비슷해보였을 것이네. 하지만 이더리움 DApp에는 일반적인 애플리케이션과는 다른 여러가지 특징이 있지.
+
+// 첫째로, 자네가 이더리움에 컨트랙트를 배포하고 나면, 컨트랙트는 변하지 않는다네(Immutable). 다시 말하자면 컨트랙트를 수정하거나 업데이트할 수 없다는 것이지.
+// 자네가 컨트랙트로 배포한 최초의 코드는 항상, 블록체인에 영구적으로 존재한다네. 
+// 이것이 바로 솔리디티에 있어서 보안이 굉장히 큰 이슈인 이유라네. 만약 자네의 컨트랙트 코드에 결점이 있다면, 그것을 이후에 고칠 수 있는 방법이 전혀 없다네. 
+// 자네는 사용자들에게 결점을 보완한 다른 스마트 컨트랙트 주소를 쓰라고 말하고 다녀야 할 것이네.
+
+// 그러나 이것 또한 스마트 컨트랙트의 한 특징이네. 코드는 곧 법인 것이지. 
+// 자네가 어떤 스마트 컨트랙트의 코드를 읽고 검증을 했다면, 자네는 자네가 함수를 호출할 때마다, 코드에 쓰여진 그대로 함수가 실행될 것이라고 확신할 수 있네. 
+// 그 누구도 배포 이후에 함수를 수정하거나 예상치 못한 결과를 발생시키지 못한다네.
+
+// 외부 의존성.
+
+// 레슨 2에서, 우리는 크립토키티 컨트랙트의 주소를 우리 DApp에 직접 써넣었네. 그런데 만약 크립토키티 컨트랙트에 버그가 있었고, 누군가 모든 고양이들을 파괴해버렸다면 어떻게 될 것 같은가?
+// 그럴 일은 잘 없겠지만, 만약 그런 일이 발생한다면 우리의 DApp은 완전히 쓸모가 없어질 것이네. 우리 DApp은 주소를 코드에 직접 써넣기 때문에 어떤 고양이들도 받아올 수 없겠지. 
+// 우리 좀비들은 고양이를 먹을 수 없을 것이고, 우리는 그걸 고치기 위해 우리의 컨트랙트를 수정할 수도 없을 것이네.
+// 이런 이유로, 대개의 경우 자네가 자네 DApp의 중요한 일부를 수정할 수 있도록 하는 함수를 만들어놓는 것이 합리적이겠지.
+// 예를 들자면 우리 DApp에 크립토키티 컨트랙트 주소를 직접 써넣는 것 대신, 언젠가 크립토키티 컨트랙트에 문제가 생기면 해당 주소를 바꿀 수 있도록 해주는 setKittyContractAddress 함수를 만들 수 있을 것이네.
+
+// 직접 해보기.
+// 레슨 2에서 우리가 만든 코드를 크립토키티 컨트랙트 주소의 업데이트가 가능하도록 바꿔보세.
+// 1. 우리가 직접 주소를 써넣었던 ckAddress가 있는 줄을 지우게.
+// 2. 우리가 kittyContract를 생성했던 줄을 변수 선언만 하도록 변경하게 - 어떤 것도 대입을 하지 않도록 하게.
+// 3. setKittyContractAddress라는 이름의 함수를 생성하게. 이 함수는 address 타입의 변수 _address를 하나의 인자로 받고, external 함수여야 하네.
+// 4. 함수 내에서, kittyContract에 KittyInterface(_address)를 대입하는 한 줄의 코드를 작성하게.
+
 import "./zombiefactory.sol";
 
 interface KittyInterface {
@@ -25,18 +51,20 @@ interface KittyInterface {
 
 contract ZombieFeeding is ZombieFactory {
 
-  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-  KittyInterface kittyContract = KittyInterface(ckAddress);
+  // 1. 이 줄을 지우게:
+  // address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+  // 2. 여기서 대입을 빼고 그냥 선언으로 바꾸게:
+  KittyInterface kittyContract;
 
-  // 여기에 있는 함수 정의를 변경
-  //  _species에만 memory가 들어가는 이유. _species는 문자열 즉, 복합 데이터 타입이다. 그리고 함수의 매개변수는 기본적으로 memory에 저장되어야 하는데, 복합 데이터(매핑, 구조체, 배열 등)는 이를 따로 명시해줘야 한다.
+  // 3. 여기 setKittyContractAddress 메소드를 추가하게
+  function setKittyContractAddress(address _address) external {
+    kittyContract = KittyInterface(_address);
+  }
   function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
-    // 여기에 if 문 추가
-// keccak256 안에 abi.encodePacked를 사용하여야 한다. (https://frontalnh.github.io/categories/ethereum/blockchain/) -> 2년전 글이라 abi.encodePacked(arg); 사용이 선택이라 명시했지만 지금은 필수인듯 하다.
     if(keccak256(abi.encodePacked(_species)) == keccak256("kitty")){
       newDna = newDna - newDna % 100 + 99;
     }
@@ -46,7 +74,6 @@ contract ZombieFeeding is ZombieFactory {
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
     uint kittyDna;
     (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-    // 여기에 있는 함수 호출을 변경: 
     feedAndMultiply(_zombieId, kittyDna, "kitty");
   }
 
