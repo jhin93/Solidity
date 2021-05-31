541334de9d1e3a300788d2d9ab1c08a0eb1c7cc8
jhin93 <scene1993@gmail.com>
2021-05-31 21:37:48 +0900

    lesson 3 챕터 2: 소유 가능한 컨트랙트 3

--- cryptoZombie/course2/zombiefeeding.sol
+++ cryptoZombie/course2/zombiefeeding.sol
@@ -13,6 +13,11 @@ pragma solidity ^0.7.0;
 // 우리는 우리 컨트랙트에서 이 주소를 바꿀 수 있게끔 하고 싶지만, 그렇다고 모든 사람이 주소를 업데이트할 수 있기를 원하지는 않네.
 // 이런 경우에 대처하기 위해서, 최근에 주로 쓰는 하나의 방법은 컨트랙트를 소유 가능하게 만드는 것이네. 컨트랙트를 대상으로 특별한 권리를 가지는 소유자가 있음을 의미하는 것이지.
 
+// 직접 해보기
+// 우리가 먼저 Ownable 컨트랙트의 코드를 ownable.sol이라는 새로운 파일로 복사해놨다네. 어서 ZombieFactory가 이걸 상속받도록 만들어보게.
+// 1. 우리 코드가 ownable.sol의 내용을 import하도록 수정하게. 어떻게 하는지 기억이 나지 않는다면 zombiefeeding.sol을 살펴보게.
+// 2. ZombieFactory 컨트랙트가 Ownable을 상속하도록 수정하게. 다시 말하지만, 이걸 어떻게 하는지 잘 기억나지 않는다면 zombiefeeding.sol을 살펴보게.
+
 import "./zombiefactory.sol";
 
 interface KittyInterface {
