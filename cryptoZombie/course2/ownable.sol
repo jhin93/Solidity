1d52a5d8a1012de2ef44eac11081898310455bc9
jhin93 <scene1993@gmail.com>
2021-06-03 21:14:51 +0900

    Ownable.sol 추가. 에러발생 및 해결. 더이상 생성자에 가시성 개념 사용 x

---
+++ cryptoZombie/course2/ownable.sol
@@ -0,0 +1,42 @@
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+
+  // 에러발생 -> 해결. 7.5버전부터 생성자에 public, external같은 가시성 개념을 사용하지 않는다고 함.
+  // https://docs.soliditylang.org/en/v0.7.5/070-breaking-changes.html#functions-and-events
+  constructor () {
+    owner = msg.sender;
+  }
+
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+}
