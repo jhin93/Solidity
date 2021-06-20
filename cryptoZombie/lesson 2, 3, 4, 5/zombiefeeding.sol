b2780ba365acbbf9c9dbdf0f8d4556a089c0e820
jhin93 <scene1993@gmail.com>
2021-06-20 20:39:41 +0900

    lesson 5 챕터 4: 리팩토링 완료

--- cryptoZombie/lesson 2, 3, 4, 5/zombiefeeding.sol
+++ cryptoZombie/lesson 2, 3, 4, 5/zombiefeeding.sol
@@ -8,6 +8,27 @@ pragma solidity ^0.7.0;
 
 import "./zombiefactory.sol";
 
+/* 챕터 4: 리팩토링
+오 이런! 우린 방금 우리의 코드에 컴파일이 안 되도록 하는 에러를 만들었네. 자네 눈치 챘었나?
+이전 챕터에서 우리는 ownerOf라는 함수를 정의했네. 하지만 레슨 4에서, 우리는 zombiefeeding.sol에서 ownerOf와 똑같은 이름의 modifier를 만들었네.
+자네가 이 코드를 컴파일하려 한다면, 컴파일러가 똑같은 이름의 제어자와 함수를 가질 수 없다며 에러를 만들어낼 것이네.
+
+그렇다면 우리가 ZombieOwnership의 함수 이름을 다른 걸로 바꿔야 할까?
+
+아니, 그렇게 할 수는 없네!!! 우리는 ERC721 토큰 표준을 사용하고 있음을 기억하게. 이 말인즉 다른 컨트랙트들이 우리의 컨트랙트가 정확한 이름으로 정의된 함수들을 가지고 있을 것이라 예상한다는 것이네. 
+그게 바로 이런 표준이 유용하게끔 하는 것이니 말이야 - 만약 우리 컨트랙트는 ERC721을 따른다는 것을 다른 컨트랙트가 안다면, 이 다른 컨트랙트는 우리의 내부 구현 로직을 모르더라도 우리와 통신할 수 있네.
+그러니 우리는 레슨 4에서 만든 우리의 코드에서 modifier의 이름을 다른 것으로 바꾸도록 리팩토링을 해야 하네.
+
+_직접 해보기
+zombiefeeding.sol로 돌아와서, 우리의 modifier의 이름을 ownerOf에서 onlyOwnerOf로 바꿀 것이네.
+
+1. 제어자를 정의하는 이름을 onlyOwnerOf로 바꾸게.
+2. 이 제어자를 사용하는 feedAndMultiply 함수로 스크롤을 내리게. 여기서도 그 이름을 바꿔야 할 것이네.
+
+| 참고: 우리는 이 제어자를 zombiehelper.sol과 zombieattack.sol에서도 사용하네. 
+하지만 우리는 이 레슨에서 리팩토링에 시간을 너무 많이 쓰지는 않도록 할 것이야. 그래서 내가 자네를 위해 이 파일들에서 제어자의 이름을 먼저 변경해 놓았네.
+*/
+
 interface KittyInterface {
   function getKitty(uint256 _id) external view returns (
     bool isGestating,
@@ -27,9 +48,10 @@ contract ZombieFeeding is ZombieFactory {
 
   KittyInterface kittyContract;
 
-  modifier ownerOf(uint _zombieId) {
-      require(msg.sender == zombieToOwner[_zombieId]);
-      _;
+  // 1. 제어자의 이름을 `onlyOwnerOf`로 바꾸게.
+  modifier onlyOwnerOf(uint _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    _;
   }
 
   function setKittyContractAddress(address _address) external onlyOwner {
@@ -45,7 +67,7 @@ contract ZombieFeeding is ZombieFactory {
 
   // string에 memory를 사용해야 하는 이유. 
   // solidity 0.5.0 버전부터는 구조체, 배열 또는 매핑 등의 모든 변수를 위해 데이터 위치를 명시하는 것이 필수. bytes 와 string 타입의 변수는 특별한 형태의 배열입니다(https://solidity-kr.readthedocs.io/ko/latest/types.html#arrays).
-  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal ownerOf(_zombieId){
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal onlyOwnerOf(_zombieId) {
     Zombie storage myZombie = zombies[_zombieId];
     require(_isReady(myZombie)); 
     _targetDna = _targetDna % dnaModulus;
--- cryptoZombie/lesson 2, 3, 4, 5/zombieownership.sol
+++ cryptoZombie/lesson 2, 3, 4, 5/zombieownership.sol
@@ -1,33 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.7.0;
 
-/* 챕터 3: balanceOf & ownerOf
-훌륭해, 이제 ERC721 구현을 시작해보세!
-내가 먼저 이번 레슨에서 구현할 모든 함수의 기본 구조를 복사해 놓았네.
-이 챕터에서는, 우리는 첫 두 메소드를 구현할 것이네: balanceOf와 ownerOf 말이지.
-
-* balanceOf
-function balanceOf(address _owner) public view returns (uint256 _balance);
-이 함수는 단순히 address를 받아, 해당 address가 토큰을 얼마나 가지고 있는지 반환하네.
-이 경우, 우리의 "토큰"은 좀비들이 되겠지. 우리의 DApp에서 어떤 소유자가 얼마나 많은 좀비를 가지는지 저장해놓은 곳을 기억하는가?
-
-* ownerOf
-function ownerOf(uint256 _tokenId) public view returns (address _owner);
-이 함수에서는 토큰 ID(우리의 경우에는 좀비 ID)를 받아, 이를 소유하고 있는 사람의 address를 반환하네.
-다시 말하지만, 이들은 구현하기가 매우 수월하네. 우리가 이 정보를 저장하는 mapping을 우리 DApp에 이미 가지고 있기 떄문이지. 
-이 함수들은 단 한 줄로 구현할 수 있네. return 문장 하나만 가지고 말이야.
-
-| 참고 : uint256은 uint와 동일하다는 것을 기억하게. 우리 코드에서 지금까지 uint를 사용해왔지만, 여기서는 우리가 스펙을 복사/붙여넣기 했으니 uint256을 쓸 것이네.
-
-_직접 해보기
-이 두 함수를 어떻게 구현할지 직접 생각하고 이해해 보게. 각각의 함수는 return을 쓰는 딱 1줄의 코드로만 구성되어야 하네. 
-이전 레슨들에서 우리의 코드를 살펴보고 우리가 이 데이터들을 어디에 저장하는지 확인해보게. 찾기 너무 힘들다면, "정답 보기" 버튼을 눌러 도움을 받게.
-
-1. _owner가 가진 좀비의 수를 반환하도록 balanceOf를 구현하게.
-2. ID가 _tokenId인 좀비를 가진 주소를 반환하도록 ownerOf를 구현하게.
-
-*/
-
 import "./zombieattack.sol";
 import "./erc721.sol";
 
